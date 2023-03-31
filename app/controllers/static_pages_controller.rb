9f6ba56cb1b61519356f14dbf4f39b4f25846cab
Tygh Walters <tyghwalters@gmail.com>
2023-03-30 22:47:33 -0700

    Fix Begin/Rescue Block for Exceptions
    Display message if userr has 0 public photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,26 +3,25 @@ class StaticPagesController < ApplicationController
   require 'flickr'
   
   def index 
-    begin
     flickr_params
-    # make API connection / Instantiate a new object
-    flickr = Flickr.new
-    unless params[:flickr_user_id].blank?
-      @photos = flickr.photos.search(user_id: params[:flickr_user_id])
-    else
-      @photos = flickr.photos.getRecent
-    end 
-  end
-
-  rescue StandardError => e
-    flash[:alert] = "#{e.class}: #{e.message}. Please try again..."
-    redirect_to root_path
-  end
+    begin
+      # make API connection / Instantiate a new object
+      flickr = Flickr.new
+      unless params[:flickr_user_id].blank?
+        @photos = flickr.photos.search(user_id: params[:flickr_user_id])
+      else
+        @photos = flickr.photos.getRecent
+      end 
+    rescue => e
+      flash[:alert] = "#{e.class}: #{e.message}. Please try again..."
+      redirect_to root_path
+    end
+  end  
 
   private
 
   def flickr_params
-    params.permit(:flickr_user_id)
+    params.permit(:flickr_user_id, :commit)
   end
 
 end
