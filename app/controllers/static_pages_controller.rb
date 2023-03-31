beb04668e5375de1f28417f66cf23df91bfd1006
Tygh Walters <tyghwalters@gmail.com>
2023-03-30 22:46:43 -0700

    API Call works

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,28 @@
 class StaticPagesController < ApplicationController
+  
+  require 'flickr'
+  
   def index 
-    
+    begin
+    flickr_params
+    # make API connection / Instantiate a new object
+    flickr = Flickr.new
+    unless params[:flickr_user_id].blank?
+      @photos = flickr.photos.search(user_id: params[:flickr_user_id])
+    else
+      @photos = flickr.photos.getRecent
+    end 
   end
+
+  rescue StandardError => e
+    flash[:alert] = "#{e.class}: #{e.message}. Please try again..."
+    redirect_to root_path
+  end
+
+  private
+
+  def flickr_params
+    params.permit(:flickr_user_id)
+  end
+
 end
